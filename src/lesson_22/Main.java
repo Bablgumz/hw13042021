be8a890f93edfd914dff13af64aaaf4be254f6fd
Bablgumz <aleksandrsviridov824@gmail.com>
2021-04-14 22:20:49 +0300

    .

--- src/lesson_22/Main.java
+++ src/lesson_22/Main.java
@@ -1,9 +1,16 @@
 package lesson_22;
 
+import java.util.Arrays;
+
 public class Main {
 
+    private static Object Arrays;
+
     public static void main(String[] args) {
-       
+        Arrays; {}
+
+        System.out.println();
+
 
     }
 }
